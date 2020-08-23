937d988609ca2d0eb7b88ff08b1d7c80e7b96be2
Rui LIU <xxliuruiabc@gmail.com>
2020-08-23 14:39:05 +0800

    Fix file path in mod.txt. Merge scripts.

---
+++ mod.lua
@@ -0,0 +1,4 @@
+Hooks:PostHook(WeaponFactoryTweakData, "init", "0535d0f3-87e3-4cc8-b552-351110b8a8ab", function (self)
+	self.parts.wpn_fps_upg_o_45rds.stats.gadget_zoom = 4
+	self.parts.wpn_fps_upg_o_45rds_v2.stats.gadget_zoom = 4
+end)
